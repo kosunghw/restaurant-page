bc9a3e6df795c3a04c425f20d5a4ac0e92fd77ed
Sung <sung272012@hotmail.com>
2024-08-02 22:32:41 -0400

    Add menu contents

--- src/home.js
+++ src/home.js
@@ -1,15 +1,27 @@
+import Fruits from "./images/fruits.png";
+
 export default function home() {
   const element = document.getElementById("content");
   element.innerHTML = "";
 
-  const header = document.createElement("h1");
-  header.innerText = "Rachel's Stickers";
+  const newDiv = document.createElement("div");
 
   const para = document.createElement("p");
   para.innerText = "Rachel's Stickers offers best stickers in the city";
 
-  element.appendChild(header);
-  element.appendChild(para);
+  const fruitImage = new Image();
+  fruitImage.src = Fruits;
+  fruitImage.classList.add("home-image");
+
+  const para2 = document.createElement("p");
+  para2.innerText = "Order online!";
+
+  newDiv.appendChild(para);
+  newDiv.appendChild(fruitImage);
+  newDiv.appendChild(para2);
+  newDiv.classList.add("content");
+
+  element.appendChild(newDiv);
 
   return element;
 }
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,42 @@
+import Melon from "./images/melon.png";
+import Peach from "./images/peach.png";
+import Pear from "./images/pear.png";
+import Pineapple from "./images/pineapple.png";
+import Plum from "./images/plum.png";
+import Watermelon from "./images/watermelon.png";
+
 export default function menu() {
   const content = document.querySelector("#content");
   content.innerHTML = "";
 
   const menuDiv = document.createElement("div");
+  menuDiv.classList.add("menu-container");
+
+  const melonImg = new Image();
+  const peachImg = new Image();
+  const pearImg = new Image();
+  const pineappleImg = new Image();
+  const plumImg = new Image();
+  const watermelonImg = new Image();
+
+  melonImg.src = Melon;
+  peachImg.src = Peach;
+  pearImg.src = Pear;
+  pineappleImg.src = Pineapple;
+  plumImg.src = Plum;
+  watermelonImg.src = Watermelon;
+
+  const melonMenu = document.createElement("div");
+  melonMenu.classList.add("menu");
+  const melonTitle = document.createElement("h2");
+  melonTitle.innerText = "Melon Sticker";
+  const melonPara = document.createElement("p");
+  melonPara.innerText = "This cute little melon sticker will suit anywhere";
+  melonMenu.appendChild(melonImg);
+  melonMenu.appendChild(melonTitle);
+  melonMenu.appendChild(melonPara);
+
+  menuDiv.appendChild(melonMenu);
+
+  content.appendChild(menuDiv);
 }
